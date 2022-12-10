e89018f7e01ed80613bfdb5d8174630cdc1a6e42
pedrocoutinh0 <115192942+pedrocoutinh0@users.noreply.github.com>
2022-12-09 21:10:45 -0300

    tutorialflutterbloc

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:tutorialblocflutter/models/task.dart';
+
+import 'blocs/bloc_exports.dart';
+import 'screens/tasks_screen.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+    () => runApp(const MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TasksBloc()
+        ..add(AddTask(
+          task: Task(title: 'Task1'),
+        )),
+      child: MaterialApp(
+        title: 'Flutter Tasks App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TasksScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../blocs/bloc_exports.dart';
+import '../models/task.dart';
+import '../widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  TasksScreen({Key? key}) : super(key: key);
+
+  TextEditingController titleController = TextEditingController();
+
+  void _addTask(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskScreen(titleController: titleController),
+              ),
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> tasksList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Tasks App'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              )
+            ],
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Chip(
+                  label: Text(
+                    'Tasks:',
+                  ),
+                ),
+              ),
+              TasksList(tasksList: tasksList)
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _addTask(context),
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class AddTaskScreen extends StatelessWidget {
+  const AddTaskScreen({
+    Key? key,
+    required this.titleController,
+  }) : super(key: key);
+
+  final TextEditingController titleController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(children: [
+        const Text(
+          'Add Task',
+          style: TextStyle(fontSize: 24),
+        ),
+        const SizedBox(
+          height: 1,
+        ),
+        TextField(
+          autofocus: true,
+          controller: titleController,
+          decoration: const InputDecoration(
+            label: Text('Title'),
+            border: OutlineInputBorder(),
+          ),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text('cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            var task = Task(
+              title: titleController.text,
+            );
+            context.read<TasksBloc>().add(AddTask(task: task));
+            Navigator.pop(context);
+          },
+          child: const Text('Add'),
+        ),
+      ]),
+    );
+  }
+}
